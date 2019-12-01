bf0c3f66f84852b24973376580dec3ed98ecb340
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-11-30 20:52:33 -0500

    social icons added to the footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,9 +2,19 @@
     <div id="app-footer">
         <div class="row" id="footer-content">
             <div class="col-12 col-sm-6 col-md-4">
-                <div class="text-center" id="logo-div">INSERT LOGO HERE</div>
+                
+                    <div class="text-center" id="logo-div">INSERT LOGO HERE</div>
+                    <div class="text-center" id="social-media">
+                        <font-awesome-icon class="social-icon" icon="user" />
+                        <font-awesome-icon class="social-icon" icon="envelope" />
+                        <font-awesome-icon class="social-icon" :icon="{ prefix: 'fab', iconName: 'twitter' }"/>
+                        <font-awesome-icon class="social-icon" :icon="{ prefix: 'fab', iconName: 'facebook' }"/>
+                        <font-awesome-icon class="social-icon" :icon="{ prefix: 'fab', iconName: 'instagram' }"/>
+                    </div>
+                
+
             </div>
-            <div class="col-12 col-sm-6 col-md-4 offset-md-2 col-lg-3 offset-lg-3">
+            <div class="col-12 col-sm-6 col-md-4 offset-md-2 col-lg-3 offset-lg-3 contactenos-div">
                 <h3>Contactenos</h3>
                 <p>Direccion: Cra 38A #61-09 </p>
                 <p>Telefono: 57-1-6310678 </p>
@@ -39,6 +49,7 @@
     }
     #footer-text {
         margin: auto;
+        padding: 2%;
         font-weight: 600;
     }
     #copyright {
@@ -46,8 +57,22 @@
         font-weight: 400;
     }
     #logo-div {
-        margin-top: 3%;
+        margin-top: 4%;
+        display: block;
+        padding: 2%;
     }
+    #social-media {
+        margin-top: 3%;
+        font-size: 1.5em;
+    }
+    .social-icon {
+        margin-left: 1em;
+        color: #dd4c39
+    }
+    .contactenos-div {        
+        padding: 2% 4%;
+    }
+  
     
 
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faEnvelope)
+library.add(faUser)
+library.add(faFacebook)
+library.add(faTwitter)
+library.add(faInstagram)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
